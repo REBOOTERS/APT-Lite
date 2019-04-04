66d82ff1a6188e2661702e106a65ca7d81ff235d
rookie <yingkongshi11@gmail.com>
2019-04-04 10:35:26 +0800

    migrate to androidx

--- example/src/main/java/com/engineer/aptlite/MainActivity.java
+++ example/src/main/java/com/engineer/aptlite/MainActivity.java
@@ -1,8 +1,8 @@
 package com.engineer.aptlite;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.widget.Button;
 import android.widget.TextView;
 
